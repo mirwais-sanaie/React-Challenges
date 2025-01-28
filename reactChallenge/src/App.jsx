8896a4379caf5005e347d1c301fcfd946f17a0f1
Mirwais <mirwais.sanaie@gmail.com>
2025-01-28 22:50:12 +0430

    Starting day 3 (File Explorer)

--- reactChallenge/src/App.jsx
+++ reactChallenge/src/App.jsx
@@ -1,11 +1,13 @@
 // import TabsChange from "./components/Day-1/TabsChange";
-import DataTable from "./components/Day-2/DataTable";
+// import DataTable from "./components/Day-2/DataTable";
+import FileExplorer from "./components/Day-3(File Explorer)/FileExplorer";
 
 function App() {
   return (
     <div>
       {/* <TabsChange /> */}
-      <DataTable />
+      {/* <DataTable /> */}
+      <FileExplorer />
     </div>
   );
 }
---
+++ reactChallenge/src/components/Day-3(File Explorer)/FileExplorer.jsx
@@ -0,0 +1,54 @@
+// const data = [
+//     {
+//       id: 1,
+//       name: 'README.md',
+//     },
+//     {
+//       id: 2,
+//       name: 'Documents',
+//       children: [
+//         {
+//           id: 3,
+//           name: 'Word.doc',
+//         },
+//         {
+//           id: 4,
+//           name: 'Powerpoint.ppt',
+//         },
+//       ],
+//     },
+//     {
+//       id: 5,
+//       name: 'Downloads',
+//       children: [
+//         {
+//           id: 6,
+//           name: 'unnamed.txt',
+//         },
+//         {
+//           id: 7,
+//           name: 'Misc',
+//           children: [
+//             {
+//               id: 8,
+//               name: 'foo.txt',
+//             },
+//             {
+//               id: 9,
+//               name: 'bar.txt',
+//             },
+//           ],
+//         },
+//       ],
+//     },
+//   ];
+
+function FileExplorer() {
+  return (
+    <div>
+      <h1>FILE EXPLORER</h1>
+    </div>
+  );
+}
+
+export default FileExplorer;
